cf53c77060eff3d7e746458640bd292df5bd1bfe
omonsoanasi <emmanuelomonso@gmail.com>
2022-08-30 11:27:41 +0300

    custom widgets and api endpoints

--- app/Filament/Resources/CountryResource/RelationManagers/EmployeesRelationManager.php
+++ app/Filament/Resources/CountryResource/RelationManagers/EmployeesRelationManager.php
@@ -2,7 +2,13 @@
 
 namespace App\Filament\Resources\CountryResource\RelationManagers;
 
+use App\Models\City;
+use App\Models\Country;
+use App\Models\State;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
@@ -20,9 +26,41 @@ class EmployeesRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('first_name')
-                    ->required()
-                    ->maxLength(255),
+                TextInput::make('first_name')->required()->maxLength(100),
+                TextInput::make('last_name')->required()->maxLength(100),
+                TextInput::make('address')->required()->maxLength(255),
+                Select::make('department_id')
+                    ->relationship('department', 'name')->required(),
+                Select::make('country_id')
+                    ->label('Country')
+                    ->options(Country::all()->pluck('name','id'))
+                    ->reactive()
+                    ->afterStateUpdated(fn (callable $set) => $set('state_id',null))->required(),
+                Select::make('state_id')
+                    ->label('State')
+                    ->options(function (callable $get){
+                        $country = Country::find($get('country_id'));
+                        if(!$country){
+                            return State::all()->pluck('name','id');
+                        }
+                        return $country->states->pluck('name','id');
+                    })
+                    ->reactive()
+                    ->afterStateUpdated(fn (callable $set) => $set('city_id',null))->required(),
+                Select::make('city_id')
+                    ->label('City')
+                    ->options(function (callable $get){
+                        $state = State::find($get('state_id'));
+                        if(!$state){
+                            return City::all()->pluck('name','id');
+                        }
+                        return $state->city->pluck('name','id');
+                    })
+                    ->reactive()->required(),
+//                            ->afterStateUpdated(fn (callable $set) => $set('city_id',null)),
+                TextInput::make('zip_code')->required(),
+                DatePicker::make('birth_date')->required(),
+                DatePicker::make('date_hired')->required(),
             ]);
     }
 
@@ -45,5 +83,5 @@ class EmployeesRelationManager extends RelationManager
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
-    }    
+    }
 }
---
+++ app/Http/Resources/EmployeeResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'firstName' => $this->first_name,
+            'lastName' => $this->last_name,
+            'countryId' => $this->country_id,
+        ];
+    }
+}
